49470935821a693b29553c7c05a2519ac5694cf9
Ian Vidmar <68394559+DRVR1@users.noreply.github.com>
2025-04-06 23:26:19 -0300

    Avances en el login
    - Toast
    - Validacion

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Manejo de padding para los insets del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Referencias a los inputs y el botón
+        val etUsername = findViewById<EditText>(R.id.etUsername)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        // Acción del botón
+        btnLogin.setOnClickListener {
+            val username = etUsername.text.toString()
+            val password = etPassword.text.toString()
+
+            // Aquí podrías validar credenciales si querés
+            //“Juan Torres” y la clave
+            //"1234utn"
+            val usuario = arrayOf("juan", "utn1234")
+            if(usuario[0] == username && usuario[1] == password){
+                val intent = Intent(this, pagina2::class.java)
+                intent.putExtra("username", username)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
