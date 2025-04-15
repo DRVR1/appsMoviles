b6d0c1d533c80c05b8d2c993abc34de362a04b41
zoe.quiroz3 <zoequiroz03@gmail.com>
2025-04-15 15:33:55 -0300

    funcionalidades opciones y campo de texto con leyenda.

--- app/src/main/java/com/example/myapplication/pagina2.kt
+++ app/src/main/java/com/example/myapplication/pagina2.kt
@@ -1,7 +1,12 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +23,12 @@ class pagina2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        // val radioOtra  = findViewById<RadioButton>(R.id.radioButton6)
+        val label       = findViewById<TextView>(R.id.LabelEspecifique)
+        val editText    = findViewById<EditText>(R.id.editTextOtra)
+
         val imgAndroid = findViewById<ImageView>(R.id.imgAndroid)
         val imgIos = findViewById<ImageView>(R.id.imgIos)
         imgAndroid.setOnClickListener {
@@ -26,6 +37,18 @@ class pagina2 : AppCompatActivity() {
 
         imgIos.setOnClickListener {
             Toast.makeText(this, "Elegiste iOS", Toast.LENGTH_SHORT).show()
+        }
+
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            val mostrar = checkedId == R.id.radioButton6
+            // visibilidad
+            label.visibility = if (mostrar) View.VISIBLE else View.GONE
+            editText.visibility = if (mostrar) View.VISIBLE else View.GONE
+            // habilitar o no
+            editText.isEnabled = mostrar
+            if (!mostrar) {
+                editText.text.clear()
+            }
         }
     }
 }
